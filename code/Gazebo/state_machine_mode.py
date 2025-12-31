2f91e6c472cb8c57d51dc00edcd03ecca01d0f6e
jostoelz <126093949+jostoelz@users.noreply.github.com>
2025-12-31 15:11:08 +0100

    State machine running with final surveillance mode

--- code/Gazebo/state_machine_mode.py
+++ code/Gazebo/state_machine_mode.py
@@ -3,39 +3,43 @@ import rospy
 from geometry_msgs.msg import Twist
 from mavros_msgs.msg import State
 from mavros_msgs.srv import CommandBool, SetMode
-from geometry_msgs.msg import Point, PoseStamped
 from std_msgs.msg import Bool
 
 class StateMachine:
     def __init__(self):
-        # Node
         rospy.init_node("state_machine_node")
-
-        # Publisher
+        
+        # Publisher for the final velocity command sent to the flight controller
         self.vel_pub = rospy.Publisher("/mavros/setpoint_velocity/cmd_vel_unstamped", Twist, queue_size=10)
 
-        # Subscriber
+        # Subscribers for system state and input velocity streams
         rospy.Subscriber("/mavros/state", State, self.state_cb)
         self.current_state = State()
+        
         rospy.Subscriber("/surveillance_velocity", Twist, self.surveillance_cb)
         self.surveillance_velocity = Twist()
+        
         rospy.Subscriber("/chasing_velocity", Twist, self.chasing_cb)
         self.chasing_velocity = Twist()
+        
         rospy.Subscriber("/object_detector", Bool, self.object_detector_cb)
         self.object_detected = Bool()
+        
         rospy.Subscriber("/hovering_velocity", Twist, self.hovering_cb)
         self.hovering_velocity = Twist()
 
-        # OFFBOARD & Arming Setup
+        # Service clients for arming and mode switching
         rospy.wait_for_service("/mavros/cmd/arming")
         rospy.wait_for_service("/mavros/set_mode")
         self.arming_client = rospy.ServiceProxy("/mavros/cmd/arming", CommandBool)
         self.set_mode_client = rospy.ServiceProxy("/mavros/set_mode", SetMode)
 
+        # Internal state variables
         self.last_object_seen_time = rospy.Time.now()
         self.hover_duration = rospy.Duration(3.0)
         self.hovering = False
 
+    # --- Callback Functions ---
     def state_cb(self, msg):
         self.current_state = msg
 
@@ -52,72 +56,70 @@ class StateMachine:
         self.hovering_velocity = msg
 
     def decision_maker(self):
+        """
+        Determines the appropriate velocity command based on the current
+        operational state (Surveillance, Chasing, or Hovering).
+        """
         twist = Twist()
         now = rospy.Time.now()
 
+        # The following logic handles automatic state transitions
+        '''
         if self.object_detected.data:
-            twist.linear.x = self.chasing_velocity.linear.x
-            twist.linear.y = self.chasing_velocity.linear.y
-            twist.linear.z = self.chasing_velocity.linear.z
-            twist.angular.x = self.chasing_velocity.angular.x
-            twist.angular.y = self.chasing_velocity.angular.y
-            twist.angular.z = self.chasing_velocity.angular.z
+            # CHASING MODE
+            twist = self.chasing_velocity
             self.last_object_seen_time = now
             self.hovering = False
-            rospy.loginfo("chasing modus")
+            rospy.loginfo_throttle(1, "State: Chasing")
         else:
             time_since_seen = now - self.last_object_seen_time
-
             if time_since_seen < self.hover_duration:
-                twist.linear.x = self.hovering_velocity.linear.x
-                twist.linear.y = self.hovering_velocity.linear.y
-                twist.linear.z = self.hovering_velocity.linear.z
-                twist.angular.x = self.hovering_velocity.angular.x
-                twist.angular.y = self.hovering_velocity.angular.y
-                twist.angular.z = self.hovering_velocity.angular.z
+                # HOVERING MODE (Target lost recently)
+                twist = self.hovering_velocity
                 self.hovering = True
-                rospy.loginfo("hovering modus")
-
+                rospy.loginfo_throttle(1, "State: Hovering")
             else:
+                # SURVEILLANCE MODE
                 twist.linear.x = self.surveillance_velocity.linear.x
-                twist.linear.y = self.surveillance_velocity.linear.y
+                
+                # --- Correction: Enforce zero lateral velocity ---
+                # twist.linear.y = 0.0 
+                # -------------------------------------------------
+                
                 twist.linear.z = self.surveillance_velocity.linear.z
-                twist.angular.x = self.surveillance_velocity.angular.x
-                twist.angular.y = self.surveillance_velocity.angular.y
                 twist.angular.z = self.surveillance_velocity.angular.z
                 self.hovering = False
-                rospy.loginfo("surveillance modus")
+                rospy.loginfo_throttle(1, "State: Surveillance (Y-axis restricted)")
+        '''
+        
+        # Default pass-through (Overridden logic for testing)
+        twist = self.surveillance_velocity
 
         return twist
 
     def run(self):
         rate = rospy.Rate(30)
         last_req = rospy.Time.now()
-
-        # Warte auf Verbindung zum FCU
-        while not rospy.is_shutdown() and (self.current_state is None or not self.current_state.connected):
+        
+        # Wait for the flight controller to connect
+        while not rospy.is_shutdown() and not self.current_state.connected:
             rate.sleep()
 
-        rospy.loginfo("Verbindung hergestellt. Starte OFFBOARD und Arming...")
-
+        # Main control loop
         while not rospy.is_shutdown():
-            # OFFBOARD setzen
+            # Periodically attempt to switch to OFFBOARD mode if not active
             if self.current_state.mode != "OFFBOARD" and (rospy.Time.now() - last_req) > rospy.Duration(5.0):
                 self.set_mode_client(0, "OFFBOARD")
                 last_req = rospy.Time.now()
-                rospy.loginfo("OFFBOARD gesetzt")
-
-            # Arming
+            
+            # Periodically attempt to ARM the vehicle if not armed
             elif not self.current_state.armed and (rospy.Time.now() - last_req) > rospy.Duration(5.0):
                 self.arming_client(True)
                 last_req = rospy.Time.now()
-                rospy.loginfo("Drohne ge-armed")
 
-            # Velocity berechnen und senden
             twist = self.decision_maker()
             self.vel_pub.publish(twist)
             rate.sleep()
 
 if __name__ == "__main__":
-    controller = StateMachine()
-    controller.run()
+    StateMachine().run()
