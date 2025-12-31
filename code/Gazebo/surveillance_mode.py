6fdf0c8d743ba518c78f344000c9e1184bce5899
jostoelz <126093949+jostoelz@users.noreply.github.com>
2025-12-31 10:27:02 +0100

    Working surveillance mode in Gazebo (except orientation and height control)

--- code/Gazebo/surveillance_mode.py
+++ code/Gazebo/surveillance_mode.py
@@ -11,9 +11,8 @@ class SurveillanceVelocity:
         rospy.init_node("surveillance_node")
 
         # Publisher für Velocity
-        self.vel_pub = rospy.Publisher(
-            "/surveillance_velocity", Twist, queue_size=10
-        )
+        self.vel_pub = rospy.Publisher("/surveillance_velocity", Twist, queue_size=10)
+        #self.vel_pub = rospy.Publisher("/mavros/setpoint_velocity/cmd_vel_unstamped", Twist, queue_size=10)
 
         # Subscriber für State
         rospy.Subscriber("/mavros/state", State, self.state_cb)
@@ -29,20 +28,18 @@ class SurveillanceVelocity:
         self.current_position.x = 0.0
         self.current_position.y = 0.0
         self.current_position.z = 0.0
-        self.altitude = 2.0
+        self.altitude = 4.0
 
         self.max_vel = 3.0
 
         #  Controller Gains
         self.kp_x = 0.1
         self.kp_y = 0.1
-        self.kp_z = 0.5
+        self.kp_z = 1
         self.tolerance = 0.5
 
     def state_cb(self, msg):
         self.current_state = msg
-        # Optional: aktuelle Position aus MAVROS lesen
-        # z.B. von /mavros/local_position/pose, sonst Annahme: selbstgeschätzt
 
     def pose_cb(self, msg):
         self.current_position.x = msg.pose.position.x
@@ -63,21 +60,8 @@ class SurveillanceVelocity:
             if self.current_target_index >= len(self.route):
                 self.current_target_index = 0
 
-        # PID control for height
-        # if not hasattr(self, 'z_error_sum'):
-            # self.z_error_sum = 0.0
-            # self.z_error_prev = 0.0
-        kp_z = 0.9
-        ki_z = 0.05
-        kd_z = 0.2
-        # error
-        # self.z_error_sum += dz
-        # d_error = dz - self.z_error_prev
-        # self.z_error_prev = dz
-        # z_control = kp_z * dz + ki_z * self.z_error_sum + kd_z * d_error
-        z_control = kp_z * dz
+        z_control = self.kp_z * dz
         z_control = max(min(z_control, self.max_vel), -self.max_vel)
-
 
         twist = Twist()
         twist.linear.x = max(min(dx * self.kp_x, self.max_vel), -self.max_vel)
@@ -95,8 +79,6 @@ class SurveillanceVelocity:
         # Warte auf Verbindung zum FCU
         while not rospy.is_shutdown() and (self.current_state is None or not self.current_state.connected):
             rate.sleep()
-
-        rospy.loginfo("Verbindung hergestellt.")
 
         while not rospy.is_shutdown():
             # Velocity berechnen und senden
